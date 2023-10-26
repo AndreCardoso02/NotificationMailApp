b316f24862f71aa3285c63977404dcda3ad2d376
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-10-26 12:31:31 +0100

    Notificando mais de um utilizador

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Notifications\notificaUser;
+use Illuminate\Support\Facades\Notification;
 
 class UserController extends Controller
 {
@@ -15,6 +16,9 @@ class UserController extends Controller
         $user->name = $request->name;
         $user->save();
 
-        $user->notify(new notificaUser($user));
+        // $user->notify(new notificaUser($user)); // Envia para apenas um Utilizador
+        $usuarios = User::whereNotIn('id', [$user->id])->get();
+
+        Notification::send($usuarios, new notificaUser($user)); // Está a notificar certos utilizadores que o utilizador actual foi criado
     }
 }
