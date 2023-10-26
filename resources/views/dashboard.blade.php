7afa399c69a0895118b6fcfa9b265d427a1e15a8
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-10-26 14:56:02 +0100

    Adicionando na view meios de visualizar as notificações

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,54 @@
         </h2>
     </x-slot>
 
+    <!-- Adicionando a lista de notificações recebidas -->
     <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Você está logado
+                    <br><br>
+                    @if(isset(Auth::user()->unreadNotifications))
+                        @foreach (Auth::user()->unreadNotifications as $notification)
+                            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                                <strong>Olá {{ $notification->data[0]['name'] }}</strong>
+                                {{ $notification->data['mensagem'] }}
+                                <button type="button" class="close marcar-lido" data-id="{{ $notification->id }}">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endforeach
+                    @endif
+                    <button type="button" id="marcar-todos" class="btn btn-dark">Marcar todos como lido</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @section('scripts')
+        <script>
+            function marcarLido(id) {
+                return $.ajax({
+                    url: "{{ route('marcar.lido') }}",
+                    mehtod: 'GET',
+                    data: {
+                        id: id,
+                    }
+                });
+            }
+
+            $(function() {
+                $('.marcar-lido').click(function() {
+                    let request = marcarLido($(this).data('id'));
+                    request.done(() => {
+                        $(this).parents('div.alert').remove();
+                    });
+                });
+            });
+        </script>
+    @endsection
+
+    {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -13,5 +60,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 </x-app-layout>
