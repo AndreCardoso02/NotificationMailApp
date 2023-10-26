7724939f47de9373ef8cc90f774dc373dce554c4
AndreCardoso02 <andremirandacardoso02@gmail.com>
2023-10-26 12:18:33 +0100

    Adicionando fila, conceito que permite a execução de tarefas sem bloquear a tela do utilizador

--- app/Notifications/notificaUser.php
+++ app/Notifications/notificaUser.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use App\Models\User;
 
-class notificaUser extends Notification
+class notificaUser extends Notification implements ShouldQueue
 {
     use Queueable;
     private $usuario;
